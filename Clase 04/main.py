8d575c446514ad1ebd5fbbe8acb5b32ee25d7370
gorencchi <amayaodayaka@gmail.com>
2026-05-23 11:27:02 -0600

    cambios

---
+++ Clase 04/main.py
@@ -0,0 +1,19 @@
+junio = 2
+agosto = 1
+mes = "Agosto"
+if mes == "Junio":
+    print(f"{junio} personas cumplen años en junio")
+elif mes == "Agosto":
+    if  agosto == 1:
+        print(f"{agosto} persona cumple años en agosto")
+    else:
+        print(f"{agosto} personas cumplen años en agosto")
+####cliclos 
+n = 0
+while n <= 10:
+    print(n)
+    n += 1
+##Cliclo for
+n = 0
+for n in range(0, 10):
+    print(n)

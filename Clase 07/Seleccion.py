29bd77f47240abbb0aabdb37c70efda9cf9385e0
gorencchi <amayaodayaka@gmail.com>
2026-06-13 11:06:29 -0600

    .

--- Clase 07/Seleccion.py
+++ Clase 07/Seleccion.py
@@ -4,25 +4,29 @@ class Seleccion:
         self.pais = pais
         self.confederacion = confederacion
     def agregar(self, jugador):
-        self.jugadores.append(jugador) #agrega jugadores a la lista :pp
-    
+        if jugador and jugador not in self.jugadores:
+            self.jugadores.append(jugador)  # agrega jugadores a la lista si no existen
+            return True
+        return False
+
     def eliminar(self, jugador):
-        for jugador_lista in self.jugadores:
-            if jugador_lista == jugador:
-                self.jugadores.remove(jugador_lista)#borra el jugador
-                break
-            
+        if jugador in self.jugadores:
+            self.jugadores.remove(jugador)
+            return True
+        return False
+
 pais = input("Ingrese el nombre del país: ")
 confederacion = input("Escriba la confederacion: ")
 jugador = input("Escriba el nombre del jugador: ")
 argentina = Seleccion("Argentina", "CONMEBOL")
 brasil = Seleccion("Brasil", "CONMEBOL")
 espanna = Seleccion("España", "CONMEBOL")
+seleccion = Seleccion(pais, confederacion)
 
 argentina.agregar("Lionel Messi")
 argentina.agregar("Angel Di Maria")
 brasil.agregar("Neymar")
-Seleccion.agregar(jugador)
+seleccion.agregar(jugador)
 espanna.agregar("Lamine Yamal")
 espanna.agregar("Gabi")
 print(argentina.jugadores)
@@ -30,3 +34,4 @@ print(brasil.jugadores)
 print (espanna.jugadores)
 argentina.eliminar("Angel Di Maria")
 print (argentina.jugadores)
+print (seleccion.jugadores)
---
+++ Clase 07/main.py
@@ -0,0 +1,1 @@
+from Seleccion import Seleccion
